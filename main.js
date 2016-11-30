7f10ece820650e66abeef56f3190f0a475a9d4f9
simenhioa <simenhioa@users.noreply.github.com>
2016-11-30 22:32:03 +0100

    fixed eiii checker

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function scrollup(x) {
-    var test = document.querySelectorAll('#readmore-label')[x];
+    var test = document.querySelectorAll('.readmore-label')[x];
     var result = getComputedStyle(test, ':after').content;
     if (result.length < 7) {
         console.log(result.length);
